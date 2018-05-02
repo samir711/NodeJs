5a4d525a11edfd99be0bf12a0d6311cd178b2dc5
samir711 <samirdoc1@gmail.com>
2018-05-02 00:14:21 -0400

    merging changes

--- notes-node/notes.js
+++ notes-node/notes.js
@@ -3,25 +3,33 @@ console.log('Starting notes.js');
 
 const fs = require('fs');
 
+let fetchNotes = () => {
+  try {
+    var notesString = fs.readFileSync('notes-data.json');
+    return JSON.parse(notesString);
+  } catch (e) {
+    return [];
+  }
+};
+
+let saveNotes = (notes) => {
+
+  fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+
+};
+
 let addNote = (title,body) => {
-  var notes = [];
+  var notes = fetchNotes();
   var note = {
     title,
     body
   };
-  
-try {
-  var notesString = fs.readFileSync('notes-data.json');
-  notes = JSON.parse(notesString);
-} catch (e) {
-  
-  }
-  
-  let duplicateNotes = notes.filter((note) => note.title === title);
 
+  let duplicateNotes = notes.filter((note) => note.title === title);
   if (duplicateNotes.length === 0) {
     notes.push(note);
-    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+    saveNotes(notes);
+    return note;
   };
   
 };
